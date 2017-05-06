efea2e435077b6df836ec1325ebfb82987b33155
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-05-06 12:42:44 -0500

    Test: fifa_code_input.test created

---
+++ test/components/fifa_code_input.test.js
@@ -0,0 +1,18 @@
+import { renderComponent , expect } from '../test_helper';
+import FifaCodeInput from '../../src/components/fifa_code_input.js';
+
+describe('FifaCodeInput' , () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(FifaCodeInput);
+  });
+
+  it('renders something', () => {
+    expect(component).to.exist;
+  });
+
+  it("has the correct className: fifa-country-code-input", () => {
+    expect(component).to.have.class('fifa-code-input');
+  });
+});
