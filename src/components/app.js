9fdf11dc8904ed094dbbb9687daba0accd37fb06
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-05-06 12:43:34 -0500

    FifaCodeInput component has className of fifa-code-input

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
+// import FifaCodeInput from './fifa_code_input';
+
 
 const App = () => {
   return (
     <div>
-      Womans World Cup Team Matches
     </div>
   )
 }
---
+++ src/components/fifa_code_input.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class FifaCodeInput extends Component {
+  render() {
+    return (
+      <form className="fifa-code-input"></form>
+    )
+  }
+}
+
+export default FifaCodeInput;
