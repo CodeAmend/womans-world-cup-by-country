d09d3ed6a88e8b7e0c826017ddf10d93d18fceb4
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-05-07 00:57:26 -0500

    TODO: Test and API response

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -2,6 +2,7 @@ import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
 describe('App' , () => {
+
   let component;
 
   beforeEach(() => {
@@ -17,4 +18,19 @@ describe('App' , () => {
     expect(component.find('form')).to.have.class('fifa-code-input');
   })
 
+  describe("Fetch match results from API", () => {
+
+    it("User submits 'BRA' and sees results", () => {
+
+      // Type BRA in input
+      component.find('.fifa-code-input input').simulate('change', 'BRA')
+      // Submit query
+      component.find('.fifa-code-input').simulate('submit')
+
+      // TODO: HOW DO I TEST FOR API RESPONSE!!!
+
+      console.log(component.find('.loading'));
+    })
+
+  })
 });
